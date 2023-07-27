94dccf9dd84661cc210c0c567589d816536aa8c9
Rob Leroy <robleroy@msn.com>
2023-07-27 11:56:50 -0500

    after init deploy to vercel fail

--- src/routes/api/weather/+server.js
+++ src/routes/api/weather/+server.js
@@ -1,15 +1,15 @@
 import { json } from "@sveltejs/kit";
 import { WEATHER_KEY } from "$env/static/private";
-import testOneCall from "./wet_2/oneCall.json";
-import testFiveDay from "./wet_2/fiveDay.json";
+// import testOneCall from "./wet_2/oneCall.json";
+// import testFiveDay from "./wet_2/fiveDay.json";
 
 const testing = false;
 
 export async function POST({ fetch, request }) {
   let data = {};
-  if (testing) {
-    data = {...testOneCall, fiveDay: testFiveDay.list ?? []};
-  } else {
+  // if (testing) {
+  //   data = {...testOneCall, fiveDay: testFiveDay.list ?? [], timestamp: Date.now()};
+  // } else {
     const { lat, lon } = await request.json();
     try {      
       const res = await fetch(
@@ -27,11 +27,11 @@ export async function POST({ fetch, request }) {
     // console.log(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${WEATHER_KEY}`);
 
 
-      data = {...onecall, fiveDay: fiveDay.list ?? [],timestamp: Date.now()};
+      data = {...onecall, fiveDay: fiveDay.list ?? [], timestamp: Date.now()};
     } catch (error) {
       console.log("fetch error:", error);
     }
-  }
+  // }
   
   return json(data);
 }
